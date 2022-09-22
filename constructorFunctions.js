da3697445a0163cfec52fc949e0008c529d3941a
Patrick <patricksw94@gmail.com>
2022-09-22 15:30:58 -0300

    atm

---
+++ constructorFunctions.js
@@ -0,0 +1,23 @@
+function criarCelular(marcaCelular, tamanhoTela, capacidadeBateria){
+    return {
+        marcaCelular,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar() {
+            console.log("fazendo ligação...")
+        }
+    }
+}
+
+//Pascal Case - UmDoisTresQuatro
+function Celular(marcaCelular, tamanhoTela, capacidadeBateria){//propriedades sendo passada como argumento pela função
+    this.marcaCelular = marcaCelular,      //this serve para referênciar um objeto
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function() {
+        console.log("Fazendo ligação...");
+    }
+}
+
+const celular= new Celular('asus', 5.5, 5000); //new usada para instanciar todas as propriedades através de um paramêtro
+console.log(celular);
