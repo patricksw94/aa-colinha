eb58e40777638c1e2b1ac2c1668f70df15dbc64b
Patrick <patricksw94@gmail.com>
2022-09-22 00:49:08 -0300

    att

---
+++ projects/encontrarString.js
@@ -0,0 +1,15 @@
+// Criar um método para ler propriedades de um objeto e
+// exibir somente as propriedades do tipo string que estão nesse objeto
+
+const filme = {
+    titulo: 'Vingadores',
+    ano: 2018,
+    diretor: 'Robin',
+    personagem: 'Thanos'
+}
+exibirPropriedades(filme);
+function exibirPropriedades(obj) {
+    for (prop in obj) 
+    if (typeof obj[prop] === 'string')
+    console.log(prop, obj[prop]);
+}
