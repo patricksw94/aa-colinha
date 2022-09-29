d13df34c629cc07a0af3827258f93dedf20ecfb3
Patrick <patricksw94@gmail.com>
2022-09-29 12:44:13 -0300

    att

--- programadorBordo.js/functions2.js
+++ programadorBordo.js/functions2.js
@@ -1,7 +1,29 @@
 function Usuario() {
-    console.log(this);
+    this.nome = 'Patrick';
+    this.idade = 28;
+    this.soma = function(a, b) {
+        return a + b;
+    }
 }
 
-Usuario();
+var usuario = new Usuario();
+console.log(usuario);  // novo objeto referenciando uma function
 
-new Usuario();  // novo objeto referenciando uma function
+// call
+function Personagem(p1, p2, p3) {
+    console.log('personagem this', this)
+    console.log(p1, p2, p3)
+}
+
+var personagemThis = {
+    nome: 'Patrick'
+}
+
+Personagem.call(personagemThis, 'param1', 123, [5])
+
+// apply
+Personagem.apply(personagemThis, [ 'param$$$', 321, [1, 2, 3]])
+
+// bind
+var pers = Personagem.bind(personagemThis, 'Olá', 'Mundo', '!!!')
+pers('???')
